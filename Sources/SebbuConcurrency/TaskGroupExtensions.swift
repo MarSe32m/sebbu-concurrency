b878364b9efc3e2eee94b72d202487cb54f4ddb5
Sebastian Toivonen <sebastian.toivonen@gmail.com>
2026-02-19 19:15:56 +0200

    Task Group extension for Discarding variant

--- Sources/SebbuConcurrency/TaskGroupExtensions.swift
+++ Sources/SebbuConcurrency/TaskGroupExtensions.swift
@@ -7,9 +7,9 @@
 
 public extension TaskGroup {
     /// Waits for the semaphore before the task is enqueued to the TaskGroup.
-    mutating func addTask(with semaphore: AsyncSemaphore, priority: TaskPriority? = nil, operation: @escaping @Sendable () async -> ChildTaskResult) async {
+    mutating func addTask(with semaphore: AsyncSemaphore, executorPreference: (any TaskExecutor)? = nil, priority: TaskPriority? = nil, operation: @escaping @Sendable () async -> ChildTaskResult) async {
         await semaphore.wait()
-        addTask(priority: priority) {
+        addTask(executorPreference: executorPreference, priority: priority) {
             defer { semaphore.signal() }
             return await operation()
         }
@@ -18,11 +18,31 @@ public extension TaskGroup {
 
 public extension ThrowingTaskGroup {
     /// Waits for the semaphore before the task is enqueued to the ThrowingTaskGroup
-    mutating func addTask(with semaphore: AsyncSemaphore, priority: TaskPriority? = nil, operation: @escaping @Sendable () async throws -> ChildTaskResult) async {
+    mutating func addTask(with semaphore: AsyncSemaphore, executorPreference: (any TaskExecutor)? = nil, priority: TaskPriority? = nil, operation: @escaping @Sendable () async throws -> ChildTaskResult) async {
         await semaphore.wait()
-        addTask(priority: priority) {
+        addTask(executorPreference: executorPreference, priority: priority) {
             defer { semaphore.signal() }
             return try await operation()
+        }
+    }
+}
+
+public extension DiscardingTaskGroup {
+    mutating func addTask(with semaphore: AsyncSemaphore, executorPreference: (any TaskExecutor)? = nil, priority: TaskPriority? = nil, operation: @escaping @Sendable () async -> Void) async {
+        await semaphore.wait()
+        addTask(executorPreference: executorPreference, priority: priority) {
+            defer { semaphore.signal() }
+            await operation()
+        }
+    }
+}
+
+public extension ThrowingDiscardingTaskGroup {
+    mutating func addTask(with semaphore: AsyncSemaphore, executorPreference: (any TaskExecutor)? = nil, priority: TaskPriority? = nil, operation: @escaping @Sendable () async throws -> Void) async {
+        await semaphore.wait()
+        addTask(executorPreference: executorPreference, priority: priority) {
+            defer { semaphore.signal() }
+            try await operation()
         }
     }
 }
